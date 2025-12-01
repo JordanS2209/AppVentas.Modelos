ddf336be04e2aceacfcf1e7cc88c8e8646679226
JordanS2209 <jssalcedoc49@gmail.com>
2025-11-30 23:43:04 -0500

    Agregar archivos de proyecto.

---
+++ AppVentas.Modelos/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVentas.Modelos
+{
+    public class Product
+    {
+        [Key] public int ProductId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ AppVentas.Modelos/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppVentas.Modelos
+{
+    public class User
+    {
+        [Key] public int UserId { get; set; }
+        public string Nombre { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+
+
+    }
+}
